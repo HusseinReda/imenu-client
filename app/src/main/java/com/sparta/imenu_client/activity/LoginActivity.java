55d4f26a8353211771e422fede9ddaf1452915b3
__Hussein__ <hussein.abumaash@gmail.com>
2016-03-10 00:45:55 +0200

    Uploading to GitHub

--- app/src/main/java/com/sparta/imenu_client/activity/LoginActivity.java
+++ app/src/main/java/com/sparta/imenu_client/activity/LoginActivity.java
@@ -1,4 +1,4 @@
-package com.sparta.imenu_client;
+package com.sparta.imenu_client.activity;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.sparta.imenu_client.R;
 
 public class LoginActivity extends AppCompatActivity {
 
