11743a7ce190f5caa7e8c8cb108e6c3a7b15463c
__Hussein__ <hussein.abumaash@gmail.com>
2016-03-10 18:04:03 +0200

    LoginAuthService added , build.gradle modified: dependencies added and excludes added,

---
+++ app/src/main/java/com/sparta/imenu_client/service/LoginAuthService.java
@@ -0,0 +1,28 @@
+package com.sparta.imenu_client.service;
+
+import android.os.AsyncTask;
+
+import com.sparta.imenu_client.R;
+
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by Hussein Abu Maash on 3/10/2016.
+ */
+
+public class LoginAuthService extends AsyncTask<Void, Void, String> {
+    @Override
+    protected String doInBackground(Void... params) {
+        final String url = String.valueOf(R.string.url);
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
+        String result = restTemplate.getForObject(url, String.class);
+        return result;
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        //Toast.makeText(getApplicationContext(),result, Toast.LENGTH_LONG).show();
+    }
+}
